114b04ad8e83fdf10ff257a44aee4361c60b5eaa
bpeksjen <berkepeksen26@gmail.com>
2023-11-03 20:34:49 +0300

    testing once again

--- testing.r
+++ testing.r
@@ -1,18 +1,16 @@
-# Install and load the dslabs package if not already installed
-
 library(dslabs)
 
 # Load the "na_example" dataset
 data("na_example")
 
-# Display the content of the original dataset and count NAs
-head(na_example)
-cat("Total number of NAs in the original dataset:", sum(is.na(na_example)), "\n")
+# Display the original dataset as a data frame
+cat("Original Dataset (na_example) as a Data Frame:\n")
+print(as.data.frame(na_example))
 
-# Replace NAs with 0
+# Replace NAs with 0 and create a new data frame
 na_example_no_na <- na_example
 na_example_no_na[is.na(na_example_no_na)] <- 0
 
-# Display the updated dataset and count NAs in the new dataset
-head(na_example_no_na)
-cat("Total number of NAs in the new dataset (after replacing with 0):", sum(is.na(na_example_no_na)), "\n")
+# Display the updated dataset as a data frame
+cat("\nUpdated Dataset (na_example_no_na, NAs replaced with 0) as a Data Frame:\n")
+print(as.data.frame(na_example_no_na))
